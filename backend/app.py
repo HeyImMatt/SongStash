e60a4ab816819208809830fd4fdcd26bc3e65db0
heyimmatt <mrmcfarland@gmail.com>
2020-09-06 11:36:02 -0700

    Add base and index templates and a home route

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, redirect, render_template, jsonify
-from flask_login import LoginManager
-from models import db, connect_db, Annotation, Song, Stash, StashSong, User
+# from flask_login import LoginManager
+from models import db, connect_db, Annotation, Song, Stash, StashSong, User, SongAnnotation, UserSong
 from env_vars import SQLALCHEMY_DATABASE_URI, SECRET_KEY
 
 app = Flask(__name__)
-login = LoginManager(app)
+# login = LoginManager(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -12,5 +12,9 @@ app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = SECRET_KEY
 
 connect_db(app)
-db.drop_all()
-db.create_all()
+# db.drop_all()
+# db.create_all()
+
+@app.route('/')
+def home_route():
+    return render_template('index.html')
