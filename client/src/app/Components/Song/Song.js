a16434d5501880f25089266f74c757efbe2f4404
heyimmatt <mrmcfarland@gmail.com>
2020-09-11 16:24:39 -0700

    Add id for lyrics span

--- client/src/app/Components/Song/Song.js
+++ client/src/app/Components/Song/Song.js
@@ -5,7 +5,7 @@ export default function Song({ song }) {
     <>
       <h3>{song.title}</h3>
       <h5>{song.artist}</h5>
-      <span className="mt-3">{song.lyrics}</span>
+      <span id="lyrics" className="mt-3">{song.lyrics}</span>
     </>
   )
 }
