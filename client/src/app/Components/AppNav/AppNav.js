b60938188df33b8e3b3f6ab72b3a37b623563012
heyimmatt <mrmcfarland@gmail.com>
2020-09-18 11:24:26 -0700

    Add active link styling to navbar and some spacing updates

--- client/src/app/Components/AppNav/AppNav.js
+++ client/src/app/Components/AppNav/AppNav.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Nav, Navbar, NavbarBrand, NavItem, NavLink, NavbarText } from 'reactstrap';
 import { AddCircle } from '@material-ui/icons';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { CLEAR_CURRENT_SONG } from '../../Actions/types';
 import './AppNav.css';
 
 export default function AppNav() {
+  const location = useLocation();
   const dispatch = useDispatch();
   const stashes = useSelector(store => store.stash.stashes)
   const clearSong = () => {
@@ -18,24 +19,24 @@ export default function AppNav() {
   return (
     <>
       <Navbar id="main-nav-bar" color="dark" dark >
-        <NavbarBrand className="mx-auto w-100 text-center" href="/mysongs">SongStash</NavbarBrand>
-        <Nav navbar pills justified className="h-75 w-100">
-          <hr />
-          <NavItem className="mx-auto my-auto">
-            <NavbarText>My Stashes  <Link to="/stash/createstash"><AddCircle /></Link></NavbarText>
-            {stashes.map(stash => (<Link to={`/stash/${stash.id}`}><NavLink>{stash.name}</NavLink></Link>))}
+        <NavbarBrand className="mx-auto mt-0 mb-0 w-100 text-center" href="/mysongs">SongStash</NavbarBrand>
+        <hr className="mt-0 mb-2" />
+        <Nav navbar pills justified className="mt-0 h-75 w-100">
+          <NavItem className="mx-auto mt-0 mb-auto">
+            <NavbarText><u>My Stashes</u>  <Link to="/stash/createstash"><AddCircle /></Link></NavbarText>
+            {stashes.map(stash => (<Link to={`/stash/${stash.id}`}><NavLink active={location.pathname === `/stash/${stash.id}`}>{stash.name}</NavLink></Link>))}
           </NavItem>
           <hr />
           <NavItem className="mx-auto my-auto">
-            <Link to="/mysongs"><NavLink>My Songs</NavLink></Link>
+            <Link to="/mysongs"><NavLink active={location.pathname === '/mysongs'}>My Songs</NavLink></Link>
           </NavItem>
           <hr />
           <NavItem className="mx-auto my-auto">
-            <Link to="/searchsongs"><NavLink onClick={clearSong}>Song Search</NavLink></Link>
+            <Link to="/searchsongs"><NavLink active={location.pathname === '/searchsongs'} onClick={clearSong}>Song Search</NavLink></Link>
           </NavItem>
           <hr />
           <NavItem className="mx-auto my-auto">
-            <Link to="/createsong"><NavLink onClick={clearSong}>Create Song</NavLink></Link>
+            <Link to="/createsong"><NavLink active={location.pathname === '/createsong'} onClick={clearSong}>Create Song</NavLink></Link>
           </NavItem>
           <hr />
           <NavItem className="d-block mx-auto mt-auto">
